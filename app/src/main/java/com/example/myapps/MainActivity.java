c96581b4a57ab5ada9230a2809634f7b5f78cdeb
ntnlrswnd <natanaeldidi31@gmail.com>
2024-11-04 19:36:27 +0700

    Initial commit

---
+++ app/src/main/java/com/example/myapps/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.myapps;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+    }
+    public void openRegister(View view) {
+        Intent intent = new Intent(MainActivity.this, Register.class);
+        startActivity(intent);
+    }
+    public void openLogin(View view) {
+        Intent intent = new Intent(MainActivity.this, Login.class);
+        startActivity(intent);
+    }
+}
